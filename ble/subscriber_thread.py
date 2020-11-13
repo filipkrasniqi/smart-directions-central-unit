72d511656ed9c7cfa272942d7b494c5a24e7463a
Filip Krasniqi <filip.krasniqi@polimi.it>
2020-11-13 16:24:06 +0100

    Added publishing on neighbours: just saying which device is close to which node

--- ble/subscriber_thread.py
+++ ble/subscriber_thread.py
@@ -4,11 +4,12 @@ import socket
 import paho.mqtt.client as mqtt
 from ble.log_thread import LogThread
 
-BROKER_IP = "192.168.1.151" # my laptop
+BROKER_IP = "54.164.129.181";#"192.168.1.151" # my laptop
 
 class MQTTSubscriber(LogThread):
     client: mqtt.Client
-    mac_dict = {}   # dictionary of the form: {mac: {origin: <rssi_queue>}}
+    mac_dict = {}   # dictionary of the form: {mac: {origin: <rssi_queue>}}, being mac = <device sniffed>, origin = <sniffer> (rberry pi)
+    QUEUE_LENGTH = 5
 
     def on_connect(self, client, userdata, flags, rc):
         # print("Connected with result code " + str(rc))
@@ -20,21 +21,35 @@ class MQTTSubscriber(LogThread):
         return socket.gethostbyname(hostname)
 
     def on_message(self, client, userdata, msg):
-        # print("ON MESSAGE: {}".format(msg.payload))
-        origin, mac, rssi = str(msg.payload).split("$")
-        rssi = rssi[:-1]    # removing \n added by c++ code to have char*
-        if mac not in self.mac_dict:
-            self.mac_dict[mac] = {}
-        if origin not in self.mac_dict[mac]:
-            self.mac_dict[mac][origin] = collections.deque(3 * [0], 3)  # by default they are set to 0
-        self.mac_dict[mac][origin].append(int(rssi))
-        # TODO do something with this
+        splits = str(msg.payload).split("$")
+        if len(splits) >= 3:
+            origin, mac, rssi = splits
+            rssi = rssi[:-1]    # removing \n added by c++ code to have char*
+            if mac not in self.mac_dict:
+                self.mac_dict[mac] = {}
+            if origin not in self.mac_dict[mac]:
+                self.mac_dict[mac][origin] = collections.deque(self.QUEUE_LENGTH * [-100], self.QUEUE_LENGTH)  # by default they are set to 0
+            try:
+                rssi = int(rssi)
+                self.mac_dict[mac][origin].append(rssi)
+                close = 1 if (sum(list(self.mac_dict[mac][origin])) / len(self.mac_dict[mac][origin])) > -80 else 0
+                if close:
+                    print("ON MESSAGE: {}".format(mac))
+                message = "{}${}${}".format(origin, mac, close)
+                self.client.publish("ble/neighbours", message)
+            except:
+                print("ERROR: was not able to parse")
+        else:
+            print("ERROR: was not able to split")
+
+
 
     def __init__(self, name):
         LogThread.__init__(self, name)
         self.name = name
 
         self.client = mqtt.Client()
+        self.client.username_pw_set("test", "password123")
         self.client.connect(BROKER_IP, 1883, 60)
 
         self.client.on_connect = self.on_connect
