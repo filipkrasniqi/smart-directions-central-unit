47ec0d100c07c2c1705650b294783906852e7b2b
Filip Krasniqi <filip.krasniqi@polimi.it>
2020-11-25 16:41:20 +0100

    Implementation of parser

--- utils/parser.py
+++ utils/parser.py
@@ -1,21 +1,55 @@
+from map.elements.effector import Effectors
+from map.elements.node import Node
+from map.elements.nodes import Nodes
+
 class Parser:
     class __Parser:
         def __init__(self, data_dir):
             self.data_dir = data_dir
+
+        def parse_node(self, idx, node):
+            splits = node.split(" ")
+            assert len(splits) == 4, "Wrong init of node"
+            return Node(idx, float(splits[0]), float(splits[1]), splits[2], splits[3])
+
         def read_nodes(self):
+            nodes = []
             with open(self.data_dir+"nodes.txt", "r") as nodes_data:
                 for i, data in enumerate(nodes_data):
-                    if i == 0:
-    instance = None
+                    if i > 0:
+                        nodes.append(self.parse_node(i, data))
+                    else:
+                        num_nodes = int(data.replace("\n", ""))
+            adjacency = {}
+            with open(self.data_dir+"adjacency.txt", "r") as adjacency_matrix:
+                # saving adjacency as dictionary
+                for i, adjacency_row in enumerate(adjacency_matrix):
+                    adjacency[i] = []
+                    for j, val in enumerate(adjacency_row.split(" ")):
+                        if val == "1":
+                            adjacency[i].append(j)
+            return Nodes(nodes, adjacency)
+
+        def read_effectors(self):
+            effectors = []
+            with open(self.data_dir + "effectors.txt", "r") as effectors_data:
+                for i, data in enumerate(effectors_data):
+                    if i > 0:
+                        effectors.append(self.parse_node(i, data))
+                    else:
+                        num_effectors = int(data.replace("\n", ""))
+            return Effectors(effectors)
+
+    __instance = None
 
     def __init__(self, data_dir):
-        if not Parser.instance:
-            Parser.instance = Parser.__Parser()
+        if not Parser.__instance:
+            Parser.__instance = Parser.__Parser(data_dir)
         else:
-            Parser.instance.data_dir = data_dir
+            Parser.__instance.data_dir = data_dir
 
     def __getattr__(self, name):
-        return getattr(self.instance, name)
+        return getattr(self.__instance, name)
 
-    def getInstante(self):
-        return Parser.instance
+    def getInstance(self):
+        return Parser.__instance
